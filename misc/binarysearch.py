5a84507b844710a29907ae3a9981726f59e4e445
romth <roomtheater@gmail.com>
2019-08-16 11:41:19 +0900

    feat: add another variation of binary search code.

--- misc/binarysearch.py
+++ misc/binarysearch.py
@@ -50,6 +50,32 @@ def binary_search_recursive(array, target, start_index, end_index):
         return binary_search_recursive(array, target, start_index, mid_index - 1)
 
 
+def recursive_binary_search(target, source, left=0):
+    if len(source) == 0:
+        return None
+    center = (len(source)-1) // 2
+    if source[center] == target:
+        return center + left
+    elif source[center] < target:
+        return recursive_binary_search(target, source[center+1:], left+center+1)
+    else:
+        return recursive_binary_search(target, source[:center], left)
+
+
+def find_first(target, source):
+    index = recursive_binary_search(target, source)
+    if not index:
+        return None
+
+    while source[index] == target:
+        if index == 0:
+            return 0
+        if source[index - 1] == target:
+            index -= 1
+        else:
+            return index
+
+
 if __name__ == "__main__":
     array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     target = 6
@@ -63,3 +89,11 @@ if __name__ == "__main__":
     target = 4
     print(binary_search(array, target))
     print(binary_search_recursive(array, target, 0, len(array)))
+
+    multiple = [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
+    print(find_first(7, multiple))  # Should return 3
+    print(find_first(9, multiple))  # Should return None
+
+    multiple = []
+    print(find_first(7, multiple)) # Should return None
+    print(find_first(9, multiple)) # Should return None
